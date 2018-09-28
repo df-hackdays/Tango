f391417acc0b4195c84da44c8d78664a98465df5
Mark Zietara <mark@integreat.ly>
2018-09-28 19:10:22 -0400

    initial boilerplate code for middle tier created by using Spring Initializr

---
+++ live-learning/src/main/java/ca/tango/livelearning/LiveLearningApplication.java
@@ -0,0 +1,12 @@
+package ca.tango.livelearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveLearningApplication.class, args);
+    }
+}
---
+++ live-learning/src/test/java/ca/tango/livelearning/LiveLearningApplicationTests.java
@@ -0,0 +1,16 @@
+package ca.tango.livelearning;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LiveLearningApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
