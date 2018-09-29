dfcf061b475c5ba487f2cd15dda696ac4b340a59
Mark Zietara <mark@integreat.ly>
2018-09-28 20:39:33 -0400

    adding breakpoints pojos

---
+++ live-learning/src/main/java/ca/tango/livelearning/domain/AnswerBreakpoint.java
@@ -0,0 +1,11 @@
+package ca.tango.livelearning.domain;
+
+import lombok.Data;
+
+/**
+ * Only to be used by the lecturer since they need to show what the answer is
+ */
+@Data
+public class AnswerBreakpoint extends Breakpoint{
+    String answer;
+}
