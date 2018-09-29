be813158a1da74a394f80c86dd4256d2d55283b1
Mark Zietara <mark@integreat.ly>
2018-09-29 03:20:41 -0400

    changing Breakpoint to have an ID and any type of answers

--- live-learning/src/main/java/ca/tango/livelearning/domain/Breakpoint.java
+++ live-learning/src/main/java/ca/tango/livelearning/domain/Breakpoint.java
@@ -2,14 +2,17 @@ package ca.tango.livelearning.domain;
 
 import ca.tango.livelearning.enumeration.QuestionTypeEnum;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 import java.util.List;
 
 @Data
 public class Breakpoint {
+    @Id
+    String id;
     String question;
-    List<String> options;
-    long programId;
+    List<Object> options;
+    long classId;
     boolean hasCorrectAnswer = false;
     QuestionTypeEnum questionTypeEnum;
 }
