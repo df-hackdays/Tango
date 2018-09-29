43d93339d2736b3f0c37d56d252756213f5b601a
Pravat Sahoo <pravatmail@gmail.com>
2018-09-29 11:40:29 -0400

    sudentanswer id

--- live-learning/src/main/java/ca/tango/livelearning/LiveLearningApplication.java
+++ live-learning/src/main/java/ca/tango/livelearning/LiveLearningApplication.java
@@ -1,18 +1,33 @@
 package ca.tango.livelearning;
 
 import ca.tango.livelearning.domain.Breakpoint;
+import ca.tango.livelearning.domain.StudentAnswer;
+import ca.tango.livelearning.service.LiveLearningDatabaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class LiveLearningApplication {
 
+    @Autowired
+    private LiveLearningDatabaseService liveLearningDatabaseService;
     public static void main(String[] args) {
 
         SpringApplication.run(LiveLearningApplication.class, args);
+
+    }
+
+    @PostConstruct
+    public void test() {
+        StudentAnswer answer = new StudentAnswer();
+        answer.setAnswer("HTML");
+        answer.setQuestionId("123");
+        answer.setStudentId("abc");
+        liveLearningDatabaseService.insertStudentAnswer(answer);
     }
 
 }
