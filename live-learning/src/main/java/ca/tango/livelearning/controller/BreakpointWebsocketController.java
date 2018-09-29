a2953c2dd796961a5245fe5a91895884b79aa916
Mark Zietara <mark@integreat.ly>
2018-09-28 20:43:49 -0400

    initial controller to handle the websockets

---
+++ live-learning/src/main/java/ca/tango/livelearning/controller/BreakpointWebsocketController.java
@@ -0,0 +1,12 @@
+package ca.tango.livelearning.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class BreakpointWebsocketController {
+
+
+}
